aefcd057b7eceba86c9fd078080a4db10d2d9f2a
kongheng <kongheng.long14@gmail.com>
2022-02-07 23:27:34 +0700

    @Sequence

--- src/main/java/com/kongheng/spring/jpa/Model/Student.java
+++ src/main/java/com/kongheng/spring/jpa/Model/Student.java
@@ -1,7 +1,11 @@
 package com.kongheng.spring.jpa.Model;
 
+import static javax.persistence.GenerationType.SEQUENCE;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,6 +14,15 @@ import lombok.Data;
 @AllArgsConstructor
 public class Student {
   @Id
+  @SequenceGenerator(
+      name = "student_sequence",
+      sequenceName = "student_sequence",
+      allocationSize = 1
+  )
+  @GeneratedValue(
+      strategy = SEQUENCE,
+      generator = "student_sequence"
+  )
   private Long id;
   private String firstName;
   private String lastName;
