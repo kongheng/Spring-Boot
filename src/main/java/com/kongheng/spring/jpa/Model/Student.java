40d165f86de53d2fa1b9688c26e277cff02c25fb
kongheng <kongheng.long14@gmail.com>
2022-02-07 23:41:38 +0700

    @column

--- src/main/java/com/kongheng/spring/jpa/Model/Student.java
+++ src/main/java/com/kongheng/spring/jpa/Model/Student.java
@@ -2,6 +2,7 @@ package com.kongheng.spring.jpa.Model;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -23,9 +24,33 @@ public class Student {
       strategy = SEQUENCE,
       generator = "student_sequence"
   )
+  @Column(
+      name = "id",
+      updatable = false
+  )
   private Long id;
+  @Column(
+      name = "first_name",
+      nullable = false,
+      columnDefinition = "TEXT"
+  )
   private String firstName;
+  @Column(
+      name = "last_name",
+      nullable = false,
+      columnDefinition = "TEXT"
+  )
   private String lastName;
+  @Column(
+      name = "email",
+      nullable = false,
+      columnDefinition = "TEXT",
+      unique = true
+  )
   private String email;
+  @Column(
+      name = "age",
+      nullable = false
+  )
   private Integer age;
 }
